7d8d7d979ca1d955bf58167674179b8ed151a750
c0117202 <c011720223@edu.teu.ac.jp>
2020-01-24 17:47:07 +0900

    index.php

---
+++ index/index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>居酒屋</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="./media_480.css" media="screen and (max-width:480px)">
+    <link rel="stylesheet" href="./doctor_reset.css">
+</head>
+<body>
+    <header>
+        <div class="h-icon abosolute f-28">
+            <span>
+                八王子駅前居酒屋
+            </span>
+        </div>
+    </header>
+    <div>
+        
+    </div>
+</body>
+</html>
