6771435e4eb94e35ec8ea84ffc53d80cfa28db21
TamuEx <c0117208c2@edu.teu.ac.jp>
2020-03-16 22:16:43 +0900

    [add]imgをクリックした際に、凹んで膨らむアニメーションを追加

--- SP/js/index.js
+++ SP/js/index.js
@@ -1,7 +1,16 @@
-document.getElementById('anime__img').addEventListener('click',function(){
+/*document.getElementById('anime__img').addEventListener('click',function(){
   //拡大縮小
   anime({
-    targets: '.Restaurant__ItemImg',
+    targets: '#anime__img',
     scale:0.8
   });
 });
+*/
+
+function animation(ele){
+  anime({
+    targets: ele,
+    scale:0.8,
+    direction: 'alternate'
+  });
+};
