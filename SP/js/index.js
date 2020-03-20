03be9fa8af21235e95ce8d1590cbc4dce0b44dc8
TamuEx <c0117208c2@edu.teu.ac.jp>
2020-03-20 14:11:31 +0900

    [add update] ページが読み込まれた際にふわっとなるように変更 fandein_animation()に変更

--- SP/js/index.js
+++ SP/js/index.js
@@ -10,18 +10,24 @@ function animation(ele){
 };
 
 /************************
-スクロールのフェードイン
+スクロールのフェードインとロード時のフェードイン
 ************************/
 
-$(function(){
-	$(window).scroll(function (){
-		$('.fadein').each(function(){
-			var elemPos = $(this).offset().top;
-			var scroll = $(window).scrollTop();
-			var windowHeight = $(window).height();
-			if (scroll > elemPos - windowHeight){
-				$(this).addClass('scrollin');
-			}
-		});
-	});
-});
+window.onload = function(){
+  fadein_animation();
+
+  $(window).scroll(function(){
+    fadein_animation();
+  });
+
+  function fadein_animation(){
+    $('.fadein').each(function(){
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight){
+        $(this).addClass('scrollin');
+      }
+    })
+  }
+}
